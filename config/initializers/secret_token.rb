3182e398eef5aa9db87f02f8d5742497327957c0
Jordi Sala <jordism91@gmail.com>
2012-08-07 22:08:34 +0200

    Added core of Ruby on Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToDoApp::Application.config.secret_token = 'd49d5419de520a2b106ee1606b302760b7a5fae87903cf567e72dfebb0ef42a226e5c0abee07d0151ae71fbcc29dd32f3c20570d491f93beee928d1993925ba2'
